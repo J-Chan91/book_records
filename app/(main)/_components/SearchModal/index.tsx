aeced7f02a8f6579e4b7dfe67246c7e1986f5295
J-Chan91 <dev.jchan91@gmail.com>
2023-12-06 01:05:21 +0900

    메인 화면 등록한 책 없을 시 UI 수정

--- app/(main)/_components/SearchModal/index.tsx
+++ app/(main)/_components/SearchModal/index.tsx
@@ -14,17 +14,17 @@ type FormType = {
 };
 
 type Props = {
+  open: boolean;
+  onClose: () => void;
   onUpdateRecords: () => void;
 };
 
-export default function SearchModal({ onUpdateRecords }: Props) {
+export default function SearchModal({ open, onClose, onUpdateRecords }: Props) {
   const { register, handleSubmit } = useForm<FormType>();
 
   const [books, setBooks] = useState<undefined | BookType[]>(undefined);
-  const [hoverItem, setHoverItem] = useState<null | string>(null);
   const [selectItem, setSelectItem] = useState<null | BookType>(null);
 
-  const [isOpenSearchModal, setIsOpenSearchModal] = useState(false);
   const [isOpenRegisterModal, setIsOpenRegisterModal] = useState(false);
 
   const onSubmit = async ({ keyword }: FormType) => {
@@ -40,7 +40,7 @@ export default function SearchModal({ onUpdateRecords }: Props) {
   const addRecord = () => {
     onUpdateRecords();
     setIsOpenRegisterModal(false);
-    setIsOpenSearchModal(false);
+    onClose();
   };
 
   const clickItem = (item: BookType) => {
@@ -58,76 +58,68 @@ export default function SearchModal({ onUpdateRecords }: Props) {
         />
       )}
 
-      <Button
-        title="책 등록"
-        variant="primary"
-        onClick={() => setIsOpenSearchModal(true)}
-      />
-
-      {isOpenSearchModal && (
-        <Modal
-          className="p-2 w-full h-full flex sm:w-[70%] flex-col gap-2 overflow-auto sm:max-w-[70%] sm:max-h-[90%] shadow-md bg-white"
-          title="책 검색"
-          onClose={() => setIsOpenSearchModal(false)}
+      <Modal
+        className="p-2 w-full h-full flex sm:w-[70%] flex-col gap-2 overflow-auto sm:max-w-[70%] sm:max-h-[90%] shadow-md bg-white"
+        title="책 검색"
+        onClose={onClose}
+      >
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col md:flex-row items-center gap-2"
         >
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col md:flex-row items-center gap-2"
-          >
-            <input
-              className="w-full outline-none text-sm rounded transition border border-gray-200 px-2 py-2 focus:border-gray-400"
-              {...register("keyword")}
-            />
+          <input
+            className="w-full outline-none text-sm rounded transition border border-gray-200 px-2 py-2 focus:border-gray-400"
+            {...register("keyword")}
+          />
 
-            <Button
-              type="submit"
-              variant="primary"
-              title="검색"
-              className="whitespace-nowrap md:w-[10%] w-full h-full"
-            />
-          </form>
+          <Button
+            type="submit"
+            variant="primary"
+            title="검색"
+            className="whitespace-nowrap md:w-[10%] w-full h-full"
+          />
+        </form>
 
-          {books === undefined && <div />}
+        {books === undefined && <div />}
 
-          {books && !books.length ? (
-            <div>
-              <p>검색결과가 없어요</p>
-            </div>
-          ) : (
-            <div className="flex flex-col gap-2 overflow-auto">
-              {books &&
-                books.map((item) => (
-                  <div
-                    key={item.isbn}
-                    className="hover:border-gray-400 transition border border-gray-200 p-2 rounded flex items-center h-[400px]"
-                  >
-                    <ImageSection
-                      title={item.title}
-                      image={item.image}
-                      link={item.link}
-                      item={item}
-                      onClickItem={clickItem}
-                    />
+        {books && !books.length ? (
+          <div>
+            <p>검색결과가 없어요</p>
+          </div>
+        ) : (
+          <div className="flex flex-col gap-2 overflow-auto">
+            {books &&
+              books.map((item) => (
+                <div
+                  key={item.isbn}
+                  className="hover:border-gray-400 transition border border-gray-200 p-2 rounded flex items-center h-[400px]"
+                >
+                  <ImageSection
+                    title={item.title}
+                    image={item.image}
+                    link={item.link}
+                    item={item}
+                    onClickItem={clickItem}
+                  />
 
-                    <div className="overflow-auto relative w-full border cursor-default border-gray-200 rounded h-[250px] ml-2 p-2">
-                      <div className="h-full w-full py-1">
-                        <p>
-                          {item.title} - {item.author}
-                        </p>
+                  <div className="overflow-auto relative w-full border cursor-default border-gray-200 rounded h-[250px] ml-2 p-2">
+                    <div className="h-full w-full py-1">
+                      <p>
+                        {item.title} - {item.author}
+                      </p>
 
-                        <hr className="my-2 " />
+                      <hr className="my-2 " />
 
-                        <div className="text-xs text-gray-900 whitespace-pre-wrap overflow-hidden">
-                          {item.description.slice(0, 400)}...
-                        </div>
+                      <div className="text-xs text-gray-900 whitespace-pre-wrap overflow-hidden">
+                        {item.description.slice(0, 400)}...
                       </div>
                     </div>
                   </div>
-                ))}
-            </div>
-          )}
-        </Modal>
-      )}
+                </div>
+              ))}
+          </div>
+        )}
+      </Modal>
     </>
   );
 }
--- app/(main)/_components/recordsSection/index.tsx
+++ app/(main)/_components/recordsSection/index.tsx
@@ -7,12 +7,15 @@ import ProgressBar from "@/components/ProgressBar";
 import { RecordType } from "@/types/bookType";
 import SearchModal from "../searchModal";
 import { getRecords } from "@/api/book";
+import Button from "@/components/Button";
+import { cn } from "@/lib/util";
 
 type Props = {
   list: undefined | RecordType[];
 };
 export default function RecordsSection({ list }: Props) {
   const [records, setRecords] = useState<undefined | RecordType[]>([]);
+  const [isOpenSearchModal, setIsOpenSearchModal] = useState<boolean>(false);
 
   const updateRecords = async () => {
     const res = await getRecords();
@@ -27,44 +30,66 @@ export default function RecordsSection({ list }: Props) {
   }, [list]);
 
   return (
-    <div className="my-4 flex flex-col justify-center gap-2 overflow-auto">
-      <div className="w-full">
-        <SearchModal onUpdateRecords={updateRecords} />
-      </div>
+    <>
+      {isOpenSearchModal && (
+        <SearchModal
+          open={isOpenSearchModal}
+          onClose={() => setIsOpenSearchModal(false)}
+          onUpdateRecords={updateRecords}
+        />
+      )}
 
-      {!records ? (
-        <div>책을 등록해보세요</div>
-      ) : (
-        <div className="w-full justify-center grid grid-cols-1 xl:grid-cols-2 gap-2">
-          {records.map((item) => (
-            <Link
-              key={item.id}
-              className=" bg-white w-full md:flex rounded border border-gray-200 px-2 py-1 transition cursor-pointer hover:border-gray-400"
-              href={`/${item.id}`}
-            >
-              <Image
-                src={item.image}
-                alt={item.title}
-                width={240}
-                height={360}
-                className="mr-2"
-              />
+      <div
+        className={cn(
+          "my-4 h-full flex flex-col items-center gap-2 overflow-auto",
+          !records?.length && "justify-center"
+        )}
+      >
+        {!records ? (
+          <div className="gap-2 p-20 border rounded shadow flex flex-col items-center justify-center">
+            <p className="text-sm">책을 등록해보세요!</p>
 
-              <div className="w-full pt-4">
-                <ProgressBar
-                  percent={
-                    !item.current_page
-                      ? 0
-                      : Math.floor((item.current_page / item.total_page) * 100)
-                  }
+            <Button
+              title="책 등록"
+              variant="primary"
+              className="text-sm"
+              onClick={() => setIsOpenSearchModal(true)}
+            />
+          </div>
+        ) : (
+          <div className="w-full justify-center grid grid-cols-1 xl:grid-cols-2 gap-2">
+            {records.map((item) => (
+              <Link
+                key={item.id}
+                className=" bg-white w-full md:flex rounded border border-gray-200 px-2 py-1 transition cursor-pointer hover:border-gray-400"
+                href={`/${item.id}`}
+              >
+                <Image
+                  src={item.image}
+                  alt={item.title}
+                  width={240}
+                  height={360}
+                  className="mr-2"
                 />
 
-                <p className="text-sm my-2">{item.title}</p>
-              </div>
-            </Link>
-          ))}
-        </div>
-      )}
-    </div>
+                <div className="w-full pt-4">
+                  <ProgressBar
+                    percent={
+                      !item.current_page
+                        ? 0
+                        : Math.floor(
+                            (item.current_page / item.total_page) * 100
+                          )
+                    }
+                  />
+
+                  <p className="text-sm my-2">{item.title}</p>
+                </div>
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
